7e65fc8e2a86c2dd13ed3cc9a6d780635d32806e
José Truta <josetrutabonfim@gmail.com>
2024-07-26 00:32:27 -0300

    atualizando torneio.py

--- ESTRUTURA-DADOS/torneio.py
+++ ESTRUTURA-DADOS/torneio.py
@@ -1,26 +1,21 @@
-# EM ANDAMENTO
-
 t = int(input())
 
 for _ in range(t):
     n, q = [int(x) for x in input().split()]
     atl = [int(x) for x in input().split()]
 
-    rodadas = {}
+    vitorias = {}
     rod = 0
     l, r = 0, 1
-    maior = 0
     while r < len(atl):
+        if (not vitorias.get(l)): vitorias[l] = [rod, None]
+        if (not vitorias.get(r)): vitorias[r] = [rod, None]
         if (atl[l] > atl[r]):
-            rodadas[rod] = l
+            vitorias[r][1] = rod
             r += 1
-            maior = max(maior, atl[l])
-            if maior == atl[l]: idx_maior = l
         else:
-            rodadas[rod] = r
+            vitorias[l][1] = rod
             l = r
-            maior = max(maior, atl[r])
-            if maior == atl[r]: idx_maior = r
             r += 1
         rod += 1
 
@@ -28,13 +23,7 @@ for _ in range(t):
         i, k = [int(x) for x in input().split()]
         i -= 1
 
-        cont = 0
-        for j in range(min(k, len(rodadas))):
-            winner = rodadas[j]
-            if atl[winner] == maior:
-                if winner != i: break
-                else:
-                    cont += (k - j)
-                    break
-            if winner == i: cont += 1 
-        print(cont)       
+        ini, fim = vitorias[i]
+        if (k < ini): print(0)
+        elif (fim == None) or (k < fim): print(k - ini)
+        else: print(fim - ini)
