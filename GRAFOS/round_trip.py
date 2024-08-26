6516c84237c5c392d1f5f74cfe23340537b80c32
José Truta <josetrutabonfim@gmail.com>
2024-08-26 00:22:58 -0300

    desgraca

--- GRAFOS/round_trip.py
+++ GRAFOS/round_trip.py
@@ -1,5 +1,7 @@
 from collections import deque
- 
+import sys
+sys.setrecursionlimit(10**5 + 1)
+
 n, m = [int(x) for x in input().split()]
  
 adj = [[] for _ in range(n + 1)]
@@ -12,48 +14,43 @@ for i in range(m):
  
 def has_cycle(source, visited):
     queue = deque()
-    queue.append(source)
+    queue.append((source,0))
     while len(queue) > 0:
         v = queue.popleft()
-        if (visited[v]):
+        if (visited[v[0]]):
             return v
-        visited[v] = True
-        for neighbour in adj[v]:
+        visited[v[0]] = True
+        for neighbour in adj[v[0]]:
             if not visited[neighbour] and len(adj[neighbour]) > 1:
-                queue.append(neighbour)
+                queue.append((neighbour, v[0]))
     
     return -1
- 
-def dfs(source, visited):
-    stack = [(source, 0)]
-    cycle = []
-    while len(stack) > 0:
-        v = stack[-1][0]
-        if not visited[v]:
-            visited[v] = True
-            for neighbour in adj[v]:
-                if not visited[neighbour] and (len(adj[neighbour]) > 1):
-                    stack.append((neighbour, v))
-        else:
-            v = stack.pop()
-            if (len(cycle) == 0):
-                cycle.append(v)
-            else:
-                if (cycle[-1][1] == v[0]):
-                    cycle.append(v)
-    return cycle           
+
+def dfs(curr, prev, visited, familia):
+    visited[curr] = True
+    for child in adj[curr]:
+        if (child == prev): continue
+        if not visited[child]:
+            familia[child] = curr  
+            return dfs(child, curr, visited, familia)
+        return curr, child
  
  
 visited = [False for _ in range(n+1)]
 vertice = has_cycle(token, visited)
- 
+
 if (vertice == -1): print("IMPOSSIBLE")
 else:
+    familia = {}
     visited = [False for _ in range(n+1)]
-    cycle = dfs(vertice, visited) 
-    cycle.append((cycle[0][0], 0))
-    print(len(cycle))
-    for par in cycle:
-        print(par[0], end=" ")
-
-
+    fim, inicio = dfs(vertice[0], vertice[1], visited, familia)
+    ciclo = []
+    ciclo.append(inicio)
+    ciclo.append(fim)
+    curr = fim
+    while curr != inicio:
+        ciclo.append(familia[curr])
+        curr = familia[curr]
+    
+    print(len(ciclo))
+    print(*ciclo)
